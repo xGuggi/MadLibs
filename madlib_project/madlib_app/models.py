a37186a1447204b08e844a6dc27ac7a54d9095a1
Spencer Guggisberg <spencerguggi@gmail.com>
2024-02-29 14:19:53 -0700

    Third commit

--- madlib_project/madlib_app/models.py
+++ madlib_project/madlib_app/models.py
@@ -5,12 +5,13 @@ from django.db import models
 
 class Madlib(models.Model):
     name = models.CharField(max_length=50)
+    verb = models.CharField(max_length=50)
     time = models.CharField(max_length=50)
     noun = models.CharField(max_length=50)
     pronoun = models.CharField(max_length=50)
     adjective = models.CharField(max_length=50)
     place = models.CharField(max_length=50)
-    verb = models.CharField(max_length=50)
+    template_name = models.CharField(max_length=50, default='default_template.html')
 
     class Meta:
         app_label = 'madlib_app'
--- madlib_project/madlib_app/views.py
+++ madlib_project/madlib_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import MadLibForm
 from .models import Madlib
-
+from django.template import loader
 
 def madlib_form(request):
     if request.method == 'POST':
@@ -16,7 +16,8 @@ def madlib_form(request):
 
 def madlib_result(request, pk):
     madlib = Madlib.objects.get(pk=pk)
-    return render(request, 'madlib_result.html', {'madlib': madlib})
+    madlib_story = generate_madlib(madlib)
+    return render(request, 'madlib_result.html', {'madlib_story': madlib_story})
 
 
 def submit_madlib(request):
@@ -25,5 +26,21 @@ def submit_madlib(request):
         if form.is_valid():
             madlib = form.save()
             madlib_pk = madlib.pk
-            return redirect('result/', pk=madlib_pk)
+            return redirect('madlib_result', pk=madlib_pk)
     return render(request, 'madlib_app/madlib_result.html', {'form': form})
+
+
+def generate_madlib(madlib_instance):
+    context = {
+        'name': madlib_instance.name,
+        'verb': madlib_instance.verb,
+        'time': madlib_instance.time,
+        'noun': madlib_instance.noun,
+        'pronoun': madlib_instance.pronoun,
+        'adjective': madlib_instance.adjective,
+        'place': madlib_instance.place,
+    }
+
+    madlib_story = render(None,f'madlib_app/{madlib_instance.template_name}', context)
+
+    return madlib_story
