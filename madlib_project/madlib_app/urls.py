544f45b4b8ddcefbc60b199bb9d771334c677575
Spencer Guggisberg <spencerguggi@gmail.com>
2024-03-01 13:56:35 -0700

    Third Submit

--- madlib_project/madlib_app/urls.py
+++ madlib_project/madlib_app/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', madlib_form, name='madlib_form'),
-    path('result/<int:pk>/', madlib_result, name='madlib_result'),
+    path('result/', madlib_result, name='madlib_result'),
     path('submit_madlib/', views.submit_madlib, name='submit_madlib'),
 ]
--- madlib_project/madlib_app/views.py
+++ madlib_project/madlib_app/views.py
@@ -2,21 +2,27 @@ from django.shortcuts import render, redirect
 from .forms import MadLibForm
 from .models import Madlib
 from django.template import loader
+import random
+
 
 def madlib_form(request):
     if request.method == 'POST':
         form = MadLibForm(request.POST or None)
         if form.is_valid():
-            madlib = form.save()
-            return redirect('madlib_result', pk=madlib.pk)
+            form.save()
+            return redirect('madlib_result')
     else:
         form = MadLibForm()
-    return render(request, 'madlib_form.html', {'form': form})
+    return render(request, 'madlib_app/madlib_form.html', {'form': form})
 
 
-def madlib_result(request, pk):
-    madlib = Madlib.objects.get(pk=pk)
-    madlib_story = generate_madlib(madlib)
+def madlib_result(request):
+    madlib = Madlib.objects.last()
+    if madlib is None:
+        return render(request, 'madlib_app/madlib_template_1.html')
+    template_number = random.randint(1, 15)
+    template_name = f'madlib_app/madlib_template_{template_number}.html'
+    madlib_story = generate_madlib(madlib, template_name)
     return render(request, 'madlib_result.html', {'madlib_story': madlib_story})
 
 
@@ -24,13 +30,12 @@ def submit_madlib(request):
     if request.method == 'POST':
         form = MadLibForm(request.POST or None)
         if form.is_valid():
-            madlib = form.save()
-            madlib_pk = madlib.pk
-            return redirect('madlib_result', pk=madlib_pk)
+            form.save()
+            return redirect('madlib_result')
     return render(request, 'madlib_app/madlib_result.html', {'form': form})
 
 
-def generate_madlib(madlib_instance):
+def generate_madlib(madlib_instance, template_name):
     context = {
         'name': madlib_instance.name,
         'verb': madlib_instance.verb,
@@ -41,6 +46,6 @@ def generate_madlib(madlib_instance):
         'place': madlib_instance.place,
     }
 
-    madlib_story = render(None,f'madlib_app/{madlib_instance.template_name}', context)
+    madlib_story = render(None, template_name, context).content.decode('utf-8')
 
     return madlib_story
